1dab8bd8e6f290d722e512c9971fa7874c79fcc9
dgechra02 <dgrajendra2021@gmail.com>
2025-04-24 20:53:07 +0530

    resolved: made responsive + added error message

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+let count = 0;
 const apiUser = fetch("https://jsonplaceholder.typicode.com/users");
 console.log(apiUser);
 apiUser
   .then((data) => {
-    console.log ("data :", data);
+    console.log("data :", data);
     return data.json();
   })
   .then((data) => {
@@ -13,66 +14,83 @@ apiUser
 
     // when page loads at first then I want to see all the monsters at first
     const cards = document.getElementById("cards");
-      userArray.forEach((eachUser, i) => {
-        const user = document.createElement("div");
-        user.className = "user";
-        const userImage = document.createElement("img");
-        userImage.id = "user-image";
-        userImage.src = `https://robohash.org/${i}`;
-        userImage.alt = "user-image-robo";
-        const name = document.createElement("h4");
-        name.textContent = eachUser.name;
-        name.id = "name";
-        const email = document.createElement("p");
-        email.textContent = eachUser.email;
-        const username = document.createElement("p");
-        username.textContent = eachUser.username;
-        user.append(userImage, name, email, username);
-        cards.appendChild(user);
-      });
-    
+    userArray.forEach((eachUser, i) => {
+      const user = document.createElement("div");
+      user.className = "user";
+      const userImage = document.createElement("img");
+      userImage.id = "user-image";
+      userImage.src = `https://robohash.org/${i}`;
+      userImage.alt = "user-image-robo";
+      const name = document.createElement("h4");
+      name.textContent = eachUser.name;
+      name.id = "name";
+      const email = document.createElement("p");
+      email.textContent = eachUser.email;
+      const username = document.createElement("p");
+      username.textContent = eachUser.username;
+      user.append(userImage, name, email, username);
+      cards.appendChild(user);
+    });
+
     // this will filter the array accroding to the search bar
     searchInput.addEventListener("input", function () {
-      
       const cards = document.getElementById("cards");
+      count = 0; // making it zero on each render
+      let errorMessage = document.createElement("span");
       cards.innerHTML = "";
       console.log("searchInput : ", searchInput.value);
-      userArray.filter((element) => element.name.includes(searchInput.value)).forEach((eachUser, i) => {
-        const user = document.createElement("div");
-        user.className = "user";
-        const userImage = document.createElement("img");
-        userImage.id = "user-image";
-        userImage.src = `https://robohash.org/${i}`;
-        userImage.alt = "user-image-robo";
-        const name = document.createElement("h4");
-        name.textContent = eachUser.name;
-        name.id = "name";
-        const email = document.createElement("p");
-        email.id = "email";
-        email.textContent = eachUser.email;
-        const username = document.createElement("p");
-        username.id = "username";
-        username.textContent = eachUser.username;
+      userArray
+        .filter((element) => {
+          if (
+            element.name.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+            element.username.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+            element.email.toLowerCase().includes(searchInput.value.toLowerCase())
+          )
+            return element;
+          else count++;
+          console.log("count : ", count);
+        })
+        .forEach((eachUser, i) => {
+          const user = document.createElement("div");
+          user.className = "user";
+          const userImage = document.createElement("img");
+          userImage.id = "user-image";
+          userImage.src = `https://robohash.org/${i}`;
+          userImage.alt = "user-image-robo";
+          const name = document.createElement("h4");
+          name.textContent = eachUser.name;
+          name.id = "name";
+          const email = document.createElement("p");
+          email.id = "email";
+          email.textContent = eachUser.email;
+          const username = document.createElement("p");
+          username.id = "username";
+          username.textContent = eachUser.username;
 
-        user.append(userImage, name, email, username);
+          user.append(userImage, name, email, username);
 
-        cards.appendChild(user);
-      });
+          cards.appendChild(user);
+        });
+      if (count === 10) {
+        errorMessage.textContent = "No monster found! search another!!";
+        console.log("errorMessage : ", errorMessage);
+        cards.appendChild(errorMessage);
+      }
     });
   });
-  // IMPORTANT: after removing input, from input tag, when still all the monsters shows it's reason is 
-  // console.log(("Hello World!").includes("")); is true because 
-  // An empty string "" is technically present at every position in any string.
-  // Every string contains an empty string at the start, between every character, and at the end.
 
-  // console.log("Hello".includes(""));  // true
-  // console.log("World".includes(""));  // true 
-  // console.log("".includes(""));       // true
+// IMPORTANT: after removing input, from input tag, when still all the monsters shows it's reason is
+// console.log(("Hello World!").includes("")); is true because
+// An empty string "" is technically present at every position in any string.
+// Every string contains an empty string at the start, between every character, and at the end.
 
-  // Think of an empty string ("") as a placeholder that fits before, between, and after every character in "Hello World!":
-  //  "" H "" e "" l "" l "" o ""   "" W "" o "" r "" l "" d "" ! ""
-  // Since it's always present, .includes("") returns true for any string. ✅
+// console.log("Hello".includes(""));  // true
+// console.log("World".includes(""));  // true
+// console.log("".includes(""));       // true
 
+// Think of an empty string ("") as a placeholder that fits before, between, and after every character in "Hello World!":
+//  "" H "" e "" l "" l "" o ""   "" W "" o "" r "" l "" d "" ! ""
+// Since it's always present, .includes("") returns true for any string. ✅
 
 // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // const filteredArr = arr.filter((num) => num);
@@ -83,4 +101,4 @@ apiUser
 // console.log("newArr : ", newArr);
 // these are two basics through which I will filter the array according to the search
 
-console.log(("Hello World!").includes(""));
+// console.log(("Hello World!").includes(""));
